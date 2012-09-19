7aca0e3dcfccb034d8b223d12a3316ee8404449c
Matt Berridge <m.berridge@mixd.co.uk>
2012-09-19 10:10:02 +0100

    Remove extra </span> on pagination

--- inc/pagination-recipes.php
+++ inc/pagination-recipes.php
@@ -1,6 +1,6 @@
 			<?php if(show_posts_nav()): ?>
 				<nav class="pagination panel bordered filled">
-					<?php next_posts_link('<span class="older">&laquo; Older recipes</span>') ?></span>
-					<?php previous_posts_link('<span class="newer">Newer recipes &raquo;</span>') ?></span>
+					<?php next_posts_link('<span class="older">&laquo; Older recipes</span>') ?>
+					<?php previous_posts_link('<span class="newer">Newer recipes &raquo;</span>') ?>
 				</nav>
 			<?php endif; ?>
